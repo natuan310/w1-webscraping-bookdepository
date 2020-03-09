953c723ce69ccaabf03201b84bf6c959af3de589
TEE <natuan310@gmail.com>
2020-03-09 10:50:57 +0700

    Change host and port for presentation

--- app.py
+++ app.py
@@ -175,5 +175,9 @@ def search():
     return render_template('search_type.html', cate=cate, books=books[:20], categories=category)
 
 
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=8000, debug=True)
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
